29dfa7ad7a25c4138eb4e89271a4d505604151b5
KuroVine9 <kurovine9@gmail.com>
2022-11-11 00:29:14 +0900

    modified:   src/javaproj10/PanelA.java 	modified:   bin/javaproj10/FigureEditor.class 	modified:   bin/javaproj10/PanelA.class 	modified:   bin/javaproj10/PanelB.class 	modified:   bin/javaproj10/PanelC.class 	modified:   src/javaproj10/FigureEditor.java 	modified:   src/javaproj10/PanelB.java 	modified:   src/javaproj10/PanelC.java

--- src/javaproj10/PanelA.java
+++ src/javaproj10/PanelA.java
@@ -2,13 +2,35 @@ package javaproj10;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
 
 public class PanelA extends JPanel {
+    JLabel label;
 
     public PanelA() {
         setBackground(Color.YELLOW);
-        JLabel label = new JLabel("Place to Draw Graf.");
+        label = new JLabel("Place to Draw Graf.");
         add(label);
+        addMouseListener(new MyMouseListener());
+        addMouseMotionListener(new MyMouseMotionListener());
+        setLayout(null);
+        label.setSize(50, 20);
+        label.setLocation(30, 30);
+    }
+
+    class MyMouseListener extends MouseAdapter {
+        public void mouseClicked(MouseEvent e) {
+            // System.out.printf("%d, %d\n", e.getX(), e.getY());
+            label.setLocation(e.getX(), e.getY());
+        }
+    }
+
+    class MyMouseMotionListener extends MouseMotionAdapter {
+
+        public void mouseDragged(MouseEvent e) {
+            // System.out.printf("Dragged\n");
+            label.setLocation(e.getX(), e.getY());
+        }
     }
 }
